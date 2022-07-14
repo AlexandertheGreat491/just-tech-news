d8d2b56e60a258311fbb0a3c626f91cdf12d4048
agvan <agvandyke25@gmail.com>
2022-07-14 13:06:48 -0500

    added one comment

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -10,6 +10,7 @@ router.get("/", (req, res) => {
       console.log(err);
       res.status(500).json(err);
     });
+    // JavaScript equivalent of SQL query SELECT * FROM users;
 });
 
 // GET /api/users/1
