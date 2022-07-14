af3d261b37eaecaaa7bca93ecc88df3431321ca5
agvan <agvandyke25@gmail.com>
2022-07-14 13:21:11 -0500

    updated POST /api/users route using .create() method to add users

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -19,22 +19,41 @@ router.get("/:id", (req, res) => {
     where: {
       id: req.params.id,
     },
-  }).then((dbUserData) => {
-    if (!dbUserData) {
-      res.status(404).json({ message: "No user found with this id" });
-      return;
-    }
-    res.json(dbUserData);
   })
-  .catch(err => {
-    console.log(err);
-    res.status(500).json(err);
-  });
+    .then((dbUserData) => {
+      if (!dbUserData) {
+        res.status(404).json({ message: "No user found with this id" });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 //equivalent of the SQL query SELECT * FROM users WHERE id = 1
 
 // POST /api/users
-router.post("/", (req, res) => {});
+router.post("/", (req, res) => {
+  //expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+  User.create({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+  })
+    .then((dbUserData) => res.json(dbUserData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+/* equivalent to the SQL query:
+INSERT INTO users
+(username, email, password)
+VALUES
+("Lernantino", "lernantino@gmail.com", "password1234");
+ */
 
 // PUT /api/users/1
 router.put("/:id", (req, res) => {});
