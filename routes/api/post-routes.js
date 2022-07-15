f1d0be7cb082ea523e6123cdfb23c42b5ce795a5
agvan <agvandyke25@gmail.com>
2022-07-15 12:52:23 -0500

    added route for all users

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,2 +1,22 @@
+//packages and models needed for Express.js API endpoints
 const router = require("express").Router();
 const { Post, User } = require("../../models");
+
+// get all users
+router.get("/", (req, res) => {
+  console.log("======================");
+  Post.findAll({
+    attributes: ["id", "post_url", "title", "created_at"],
+    include: [
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => res.json(dbPostData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
