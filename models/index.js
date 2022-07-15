79b888a116e9b393230fe64fb981c76bce2a94cb
agvan <agvandyke25@gmail.com>
2022-07-15 12:40:34 -0500

    defined associations between Post and User

--- models/index.js
+++ models/index.js
@@ -1,5 +1,18 @@
-const User = require('./User');
+const User = require("./User");
 const Post = require("./Post");
 
-module.exports = { User, Post };
+//create associations
+User.hasMany(Post, {
+  foreignKey: "user_id",
+});
 
+/*This association creates the reference for the id
+column in the User model to link to the corresponding foreign key pair
+, which is the user_id in the Post model. */
+
+Post.belongsTo(User, {
+  foreignKey: "user_id",
+});
+// defines the relationship between the Post model and the User
+
+module.exports = { User, Post };
