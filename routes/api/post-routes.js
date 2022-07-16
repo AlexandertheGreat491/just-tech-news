846403613a1259c26aca3ea0eea859e3e58b920d
agvan <agvandyke25@gmail.com>
2022-07-16 13:47:26 -0500

    updated attributes for route

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -5,7 +5,12 @@ const sequelize = require("../../config/connection");
 // get all users
 router.get("/", (req, res) => {
   Post.findAll({
-    attributes: ["id", "post_url", "title", "created_at"],
+    attributes: ["id", 
+                 "post_url", 
+                 "title", 
+                 "created_at"
+                 [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id'), 'vote_count']
+                ],
     order: [["created_at", "DESC"]],
     include: [
       {
