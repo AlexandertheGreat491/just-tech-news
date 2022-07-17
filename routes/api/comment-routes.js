393d402e5b849adbd6dda7ca67e409b20e7528bd
agvan <agvandyke25@gmail.com>
2022-07-17 11:12:33 -0500

    updated routes

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -4,7 +4,14 @@ const commentRoutes = require("./comment-routes");
 
 router.use("/comments", commentRoutes);
 
-router.get("/", (req, res) => {});
+router.get("/", (req, res) => {
+    Comment.findAll()
+    .then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 router.post("/", (req, res) => {
   Comment.create({
@@ -19,6 +26,23 @@ router.post("/", (req, res) => {
     });
 });
 
-router.delete("/:id", (req, res) => {});
+router.delete("/:id", (req, res) => {
+    Comment.destroy({
+        where: {
+          id: req.params.id
+        }
+      })
+        .then(dbCommentData => {
+          if (!dbCommentData) {
+            res.status(404).json({ message: 'No comment found with this id!' });
+            return;
+          }
+          res.json(dbCommentData);
+        })
+        .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+        });
+});
 
 module.exports = router;
