7a48275776abed34e0bd506bfbbb8298b96b0fa8
agvan <agvandyke25@gmail.com>
2022-07-21 23:19:50 -0500

    updated the signupFormHandler function to POST the username, email, and password from the form to the server

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,6 +1,22 @@
 // this function handles the login submission
+//function can POST the username, email, and password from the form to the server
 function signupFormHandler(event) {
     event.preventDefault();
+    const username = document.querySelector('#username-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+
+    if (username && email && password) {
+        fetch('/api/users', {
+            method: 'post',
+            body: JSON.stringify({
+                username,
+                email, 
+                password
+            }),
+            headers: {'Content-Type': 'application/json'}
+        }).then((response) => {console.log(response)})
+    }
 }
 
 //listens fro the submit even from the form
