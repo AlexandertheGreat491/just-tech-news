f2e7241d6aa83cea655c85e5c6a2e0ef7d52113c
agvan <agvandyke25@gmail.com>
2022-07-21 23:29:19 -0500

    added async and await to the signupFormHandler() function

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,13 +1,13 @@
 // this function handles the login submission
 //function can POST the username, email, and password from the form to the server
-function signupFormHandler(event) {
+async function signupFormHandler(event) {
     event.preventDefault();
     const username = document.querySelector('#username-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
 
     if (username && email && password) {
-        fetch('/api/users', {
+        const response = await fetch('/api/users', {
             method: 'post',
             body: JSON.stringify({
                 username,
@@ -15,7 +15,13 @@ function signupFormHandler(event) {
                 password
             }),
             headers: {'Content-Type': 'application/json'}
-        }).then((response) => {console.log(response)})
+        });
+        //checks the response status
+        if (response.ok) {
+            console.log('success');
+        } else {
+            alert(response.statusText)
+        }
     }
 }
 
