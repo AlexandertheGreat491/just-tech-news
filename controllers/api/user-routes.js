890a2e9029540c74a8bc605cf57f4d38582b74c3
agvan <agvandyke25@gmail.com>
2022-07-22 22:50:03 -0500

    updated login post route

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -94,6 +94,12 @@ router.post("/login", (req, res) => {
       return;
     }
 
+})
+req.session.save(() => {
+  //declares session variables
+  req.session.user_id = dbUserData.id;
+  req.session.username = dbUserData.username;
+  req.session.loggedIn = true;
     res.json({ user: dbUserData, message: "You are now logged in!" });
   });
 });
