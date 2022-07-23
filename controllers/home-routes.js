2a9a019ef6b5f4731308645b5a68cd8d90e55885
agvan <agvandyke25@gmail.com>
2022-07-22 23:38:36 -0500

    server connection fixed

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,59 +1,11 @@
-const router = require("express").Router();
-const sequelize = require("../config/connection");
-const { Post, User, Comment } = require("../models");
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
 
-router.get("/", (req, res) => {
+// get all posts for homepage
+router.get('/', (req, res) => {
+  console.log('======================');
   Post.findAll({
-    attributes: [
-      "id",
-      "post_url",
-      "title",
-      "created_at",
-      [
-        sequelize.literal(
-          "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
-        ),
-        "vote_count",
-      ],
-    ],
-    include: [
-      {
-        model: Comment,
-        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
-        include: {
-          model: User,
-          attributes: ["username"],
-        },
-      },
-      {
-        model: User,
-        attributes: ["username"],
-      },
-    ],
-  })
-    .then((dbPostData) => {
-      // pass a single post object into the homepage template
-      console.log(dbPostData[0]);
-      const posts = dbPostData.map((post) => post.get({ plain: true }));
-      res.render("homepage", { posts });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
-
-//GET route that renders the login
-router.get("/login", (req, res) => {
-  res.render("login");
-});
-
-
-router.get('/post/:id', (req, res) => {
-  Post.findOne({
-    where: {
-      id: req.params.id
-    },
     attributes: [
       'id',
       'post_url',
@@ -77,19 +29,23 @@ router.get('/post/:id', (req, res) => {
     ]
   })
     .then(dbPostData => {
-      if (!dbPostData) {
-        res.status(404).json({ message: 'No post found with this id' });
-        return;
-      }
+      const posts = dbPostData.map(post => post.get({ plain: true }));
 
-      // serialize the data
-      const post = dbPostData.get({ plain: true });
-
-      // pass data to template
-      res.render('single-post', { post });
+      res.render('homepage', { posts });
     })
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
     });
 });
+
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+
+module.exports = router;
